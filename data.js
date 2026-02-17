b480d2d9647b0437ee5f0e189a9747234534df24
nikithagunde <nikithagunde@gmail.com>
2026-02-17 21:43:30 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,60 @@
+const subjectsList = [
+
+{
+name:"English",
+icon:"fas fa-book",
+channels:[
+{name:"English by Nisa",links:["https://youtube.com/playlist?list=PLUJXB7al70IAIbiajs7JqxrGqctgi80j"]},
+{name:"Sara’s Learning Point",links:["https://youtube.com/playlist?list=PLjNdhMBtsxpz5BYkI4eiiYQahD7GroZy0"]}
+]
+},
+
+{
+name:"Corporate Accounting",
+icon:"fas fa-calculator",
+channels:[
+{name:"Hasham Ali Khan",links:["https://youtube.com/playlist?list=PLWJDzVuPkXAlXTMbitIZDjiAmi7M8Zww"]},
+{name:"Devika’s Commerce",links:["https://youtube.com/playlist?list=PL32yVakBnz3KJkri5LmhkNdFuPeTmyWlt"]}
+]
+},
+
+{
+name:"Business Statistics - 2",
+icon:"fas fa-chart-line",
+channels:[
+{name:"Hasham Ali Khan",links:["https://youtube.com/playlist?list=PLWJDzVuPkXAk3twapgb0DQ9MyaQ24V4JS"]}
+]
+},
+
+{
+name:"Income Tax",
+icon:"fas fa-file-invoice-dollar",
+channels:[
+{name:"Devika’s Commerce",links:["https://youtube.com/playlist?list=PLP6CJ8yrRpA3_WbjoudHcT10AxY3Tut1B"]}
+]
+},
+
+{
+name:"Universal Human Values",
+icon:"fas fa-hands-helping",
+channels:[
+{name:"Tegron Learning Hub",links:["https://youtu.be/PZQCoQdLJmw"]}
+]
+},
+
+{
+name:"Practice of Life and General Insurance",
+icon:"fas fa-shield-alt",
+channels:[
+{name:"Dwivedi Guidance",links:["https://youtube.com/playlist?list=PLsh2FvSr3n7cD0F2FSjBVWWrVF6cR8Pla"]}
+]
+}
+
+];
+
+const data = {
+"B.Com General - Semester 4": subjectsList,
+"B.Com Honors - Semester 4": subjectsList,
+"B.Com Computer Applications - Semester 4": subjectsList,
+"B.Com Business Analytics - Semester 4": subjectsList
+};
