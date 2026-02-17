52bd4d9f89ef65ff398aa48bfd41977e45174461
nikithagunde <nikithagunde@gmail.com>
2026-02-17 21:32:24 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,61 @@
+const data = {
+
+"B.Com General - Semester 4":[
+
+{
+name:"English",
+channels:[
+{name:"English by Nisa",links:[
+"https://youtube.com/playlist?list=PLUJXB7al70IAIbiajs7JqxrGqctgi80j"
+]}
+]
+},
+
+{
+name:"Corporate Accounting",
+channels:[
+{name:"Hasham Ali Khan",links:[
+"https://youtube.com/playlist?list=PLWJDzVuPkXAlXTMbitIZDjiAmi7M8Zww"
+]}
+]
+},
+
+{
+name:"Business Statistics - 2",
+channels:[
+{name:"Hasham Ali Khan",links:[
+"https://youtube.com/playlist?list=PLWJDzVuPkXAk3twapgb0DQ9MyaQ24V4JS"
+]}
+]
+},
+
+{
+name:"Income Tax",
+channels:[
+{name:"Devika’s Commerce",links:[
+"https://youtube.com/playlist?list=PLP6CJ8yrRpA3_WbjoudHcT10AxY3Tut1B"
+]}
+]
+},
+
+{
+name:"Universal Human Values",
+channels:[
+{name:"Tegron Learning Hub",links:[
+"https://youtu.be/PZQCoQdLJmw"
+]}
+]
+},
+
+{
+name:"Practice of Life and General Insurance",
+channels:[
+{name:"Dwivedi Guidance",links:[
+"https://youtube.com/playlist?list=PLsh2FvSr3n7cD0F2FSjBVWWrVF6cR8Pla"
+]}
+]
+}
+
+]
+
+};
