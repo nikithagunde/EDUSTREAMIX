5b877ab2c5408bfde56c3bfe9ee2ec4951bfa583
nikithagunde <nikithagunde@gmail.com>
2026-02-17 20:59:29 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,57 @@
+const data = {
+
+"B.Com General - Semester 4":[
+{
+name:"English",
+channels:[
+{name:"English by Nisa",links:[
+"https://youtube.com/playlist?list=PLUJXB7al70IAIbiajs7JqxrGqc-tgi80j"
+]}
+]
+},
+{
+name:"Corporate Accounting",
+channels:[
+{name:"Hasham Ali Khan",links:[
+"https://youtube.com/playlist?list=PLWJDzVuPkXAlXTMbitIZDjiAmi7M8Zww"
+]}
+]
+},
+{
+name:"Business Statistics - 2",
+channels:[
+{name:"Devika’s Commerce",links:[
+"https://youtube.com/playlist?list=PLLhSIFfDZcUUatEzYCA8xH02JR_GlLjMA"
+]}
+]
+},
+{
+name:"Income Tax",
+channels:[
+{name:"Devika’s Commerce",links:[
+"https://youtube.com/playlist?list=PLP6CJ8yrRpA3_WbjoudHcT10AxY3Tut1B"
+]}
+]
+},
+{
+name:"Universal Human Values",
+channels:[
+{name:"Tegron Learning Hub",links:[
+"https://youtu.be/PZQCoQdLJmw"
+]}
+]
+}
+],
+
+"B.Com Computer Applications - Semester 4":[
+{
+name:"Web Technologies",
+channels:[
+{name:"Fazil Khan Tutorial",links:[
+"https://youtube.com/playlist?list=PL0IHN5oTE2R7ekgSTUPkwILoqK7oAoExo"
+]}
+]
+}
+]
+
+};
